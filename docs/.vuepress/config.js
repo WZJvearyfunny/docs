9090e446394a04786d85c5d23e294aa3f35fe642
WZJvearyfunny <824940937@qq.com>
2020-08-13 22:48:07 +0800

    完成部分样式优化以及功能新增

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,45 @@
 module.exports = {
+  title:"PIPEDREAM's live",
+  description:"这是我的个人世界，存储在互联网上，用二进制的形式永存于世",
+  head: [
+    ['link', { rel: 'icon', href: '/assets/img/favicon.ico', }],
+    ['meta', { name: 'author', content: '这是王真俊的自我描述，从24岁开始记录人生的一点一滴' }]
+  ],
   themeConfig: {
+    lastUpdated: '更新时间',
     logo: '/assets/img/logo.png',
     nav: [{
-        text: '主页',
-        link: '/'
+      text: '主页',
+      link: '/'
+    },
+    {
+      text: '个人资料',
+      link: '/about'
+    },
+    {
+      text: '项目经验',
+      ariaLabel: 'Project Menu',
+      items: [{
+        text: '工作参与项目',
+        items: [{ text: '大屏展示', link: '/language/chinese/' }, { text: '网页应用', link: '/language/chinese/' }, { text: '桌面客户端', link: '/language/chinese/' }, { text: 'Android APP', link: '/language/chinese/' }, { text: '小程序', link: '/language/chinese/' }],
       },
       {
-        text: '个人资料',
-        link: '/guide/'
-      },
-      {
-        text: '项目经验',
-        ariaLabel: 'Project Menu',
-        items: [{
-            text: '工作参与项目',
-            link: '/language/chinese/'
-          },
-          {
-            text: '个人外接项目',
-            link: '/language/japanese/'
-          }
-        ]
-      },
-      {
-        text: '社交信息',
-        link: 'https://weibo.com/u/5141902005',
-        target: '_blank',
-        rel: 'noopener noreferrer'
-      },
+        text: '个人外接项目',
+        // link: '/language/japanese/'
+        items: [{ text: "小程序", link: '' }],
+      }
+      ]
+    },
+    {
+      text: '社交信息',
+      link: 'https://weibo.com/u/5141902005',
+      target: '_blank',
+      rel: 'noopener noreferrer'
+    },
+    ],
+    sidebar: [
+      '',
+      'about'
     ]
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,15 +1,15 @@
 ---
 home: true
-# heroImage: /assets/largphoto/homePagePhoto.jpg
+heroImage: /assets/largphoto/homePagePhoto.jpg
 heroText: 王真俊的博客
-tagline: PIPEDREAM's Blog
-actionText: 游泳健身了解下
+tagline: PIPEDREAM's Live
+actionText: 游泳健身了解下→
 actionLink: /zh/guide/
 features:
 - title: 是个猛男
   details: 年芳24，相貌平平，为人亲和，自成一派。相处过的人都评价：是个猛男。 小王本人对此表示：I think so.
 - title: 求学历程
-  details: 因为忍受不了读书的苦，曾多次夸下海口要找到出去的路。一路披荆斩棘后，成为了一名大学生。就这...
+  details: 忍受不了读书的苦，夸下海口要找到出去的路，心头拥有无尽的堵，最后成了房贷的奴。(四押！)/普普通通一名大学毕业生。/就这...
 - title: 工作情况
   details: 不是软件工程相关专业毕业，入行完全是因为兴趣。在一家外包公司打磨了3年时间，习得一身好武艺（学习能力贼拉强）。996如呼吸般简单。
 footer: Noone Licensed | Copyright © 2020-present wzj
